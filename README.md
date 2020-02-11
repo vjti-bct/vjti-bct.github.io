b039856aae6278af0bbd8de7aeb5edba032852b6
abcf98 <amanframewala@gmail.com>
2020-02-11 12:14:47 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GlobalBlockchainWebsite
+
+Website made for one day International Conference on Blockchain for Global Good \
+Hosted by VJTI Mumbai 
+https://vjti-bct.in/
---
+++ social/blockchain.py
@@ -0,0 +1,15 @@
+import hashlib as hasher
+
+
+class Block:
+    private index
+    private timestamp
+    private hash_value
+    private prev_hash
+    private data
+    private nonce
+
+    def calc_hash(self, block):
+        
+
+
